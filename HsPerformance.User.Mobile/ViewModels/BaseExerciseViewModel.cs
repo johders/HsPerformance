9de51172c1802329f61e01775af14439ddb8bd3c
jd-HW <johannes.dereuddre@student.howest.be>
2024-08-04 09:23:59 +0200

    Add fallback values to xaml page

--- HsPerformance.User.Mobile/ViewModels/BaseExerciseViewModel.cs
+++ HsPerformance.User.Mobile/ViewModels/BaseExerciseViewModel.cs
@@ -9,9 +9,13 @@
         { 
             get 
             {
-                string imgLink = "https://img.youtube.com/vi/";
-                string[] temp = VideoUrl.Split(new char[] { '=', '&' });
-                return $"{imgLink}{temp[1]}/2.jpg";
+                if(VideoUrl != null)
+                {
+                    string imgLink = "https://img.youtube.com/vi/";
+                    string[] temp = VideoUrl.Split(new char[] { '=', '&' });
+                    return $"{imgLink}{temp[1]}/2.jpg";
+                }
+                return "https://answers-afd.microsoft.com/static/images/image-not-found.jpg";
             }
         }
         public string Description { get; set; }
